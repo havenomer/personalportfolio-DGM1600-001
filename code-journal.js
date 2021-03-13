dba8eb517fd1db1bec1fff2a5ab249e557e65651
Haven Omer <haven.omer@gmail.com>
2021-03-12 23:54:37 -0700

    Added to my code journal for javascript

--- code-journal.js
+++ code-journal.js
@@ -1,1 +1,9 @@
 /* Variables - containers that store values */
+
+var name // a declared variable, but not initialized and it's in the global scope (BAD)
+
+let foo // a declared variable that can be changed
+
+const bar // a declared variable that cannot be changed = short for "constant"
+
+const ANSWER = 42 // const is declared and initialized with the value 42
