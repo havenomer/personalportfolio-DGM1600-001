471d356dab0e05d82e55780da0a2875f1a8b6cf2
Haven Omer <haven.omer@gmail.com>
2021-03-13 22:37:19 -0700

    Added notes for strings, numbers, and booleans

--- code-journal.js
+++ code-journal.js
@@ -7,3 +7,27 @@ let foo // a declared variable that can be changed
 const bar // a declared variable that cannot be changed = short for "constant"
 
 const ANSWER = 42 // const is declared and initialized with the value 42
+
+//Strings
+
+let string1 = "Hello World" // Read the '=' as 'is assigned the value of...'
+
+let string2 = 'Hello Utah'
+
+let string3 = new String('Hello World!')
+
+//Number
+
+let myNum = 548143
+
+let myNum2 = 45.69
+
+'1' == 1 // This statement is trur because of type coercion and loose quality checking
+
+'1' === 1 // False because this is strict equality checking
+
+//Boolean
+
+let myBool = true
+
+
