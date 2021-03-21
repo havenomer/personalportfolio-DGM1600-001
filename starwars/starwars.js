a6912c0cad961d74a38f139e02427f5d6c51ca83
Haven Omer <haven.omer@gmail.com>
2021-03-20 22:37:37 -0600

    Added star wars data and title

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,5 +1,10 @@
 import { films } from '../data/films.js'
 
-const greeting = 'Hello World!'
 
-console.log(greeting)
+console.log(films[6])
+
+let filmOne = document.querySelector('#film1')
+let filmTwo = document.querySelector('#film2')
+
+filmOne.textContent = films[2].title
+filmTwo.textContent = films[1].title
