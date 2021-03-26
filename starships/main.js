a8edff5dc64f4401e523678730ef448eec40b1fb
Haven Omer <haven.omer@gmail.com>
2021-03-25 20:29:14 -0600

    Added bulma modal to starships page

--- starships/main.js
+++ starships/main.js
@@ -7,6 +7,18 @@ const nav = document.querySelector('nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipView')
 
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
 function populateNav(starships) {
     starships.forEach((starship) => {
         let anchorWrap = document.createElement('a')
@@ -28,10 +40,36 @@ function populateShipView(shipData) {
     shipImage.addEventListener('error', () => {
         console.log(`Oops! Image doesn't exist.`)
         shipImage.hidden = true
+        dialog.classList.toggle("is-active")
     })
     shipView.appendChild(shipImage)
 }
 
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', '#000')
+    for (let i = 0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('position', 'absolute')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', '#777')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        element.appendChild(star)
+    }
+}
+
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
 populateNav(starships)
 
-populateShipView()
+addStarField(document.querySelector('body'), 1000)
+
+//populateShipView()
