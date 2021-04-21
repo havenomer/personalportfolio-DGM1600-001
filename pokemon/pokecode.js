c4ed0c06e826621c5f6a25a94725f1c52f90ec3c
Haven Omer <haven.omer@gmail.com>
2021-04-20 21:32:54 -0600

    Added js to make images appear correctly

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -17,13 +17,46 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-  getAPIData("https://pokeapi.co/api/v2/pokemon?limit=25").then((data) => {
-    for (const singlePokemon of data.results) {
-      populatePokeCard(singlePokemon)
+  getAPIData("https://pokeapi.co/api/v2/pokemon?limit=25").then(
+    async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData)
+        )
+      }
     }
-  })
+  );
 }
 
 function populatePokeCard(singlePokemon) {
-    console.log(singlePokemon)
+  console.log(singlePokemon);
+  let pokeScene = document.createElement("div");
+  pokeScene.className = "scene";
+  let pokeCard = document.createElement("div");
+  pokeCard.className = "card";
+
+  pokeCard.appendChild(populateCardFront(singlePokemon));
+  pokeScene.appendChild(pokeCard);
+  pokeGrid.appendChild(pokeScene);
+}
+
+function populateCardFront(pokemon) {
+  let pokeFront = document.createElement("div");
+  pokeFront.className = "card__face card__face--front";
+  let frontLabel = document.createElement("p");
+  frontLabel.textContent = pokemon.name;
+  let frontImage = document.createElement("img");
+  frontImage.src = `images/${getImageFileName(pokemon)}.png`;
+
+  pokeFront.appendChild(frontLabel);
+  pokeFront.appendChild(frontImage);
+  return pokeFront;
+}
+
+function getImageFileName(pokemon) {
+  if (pokemon.id < 10) {
+    return `00${pokemon.id}`
+  } else if (pokemon.id > 9 && pokemon.id < 100) {
+    return `0${pokemon.id}`
+  }
 }
